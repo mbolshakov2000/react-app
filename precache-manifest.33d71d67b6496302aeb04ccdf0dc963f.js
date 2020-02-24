f8f3b92dfb26512ed4a8af67ee931b570a9e4d0b
Mikhail Bolshakov <michaelbolshakov@me.com>
2020-02-24 20:11:46 +0300

    Updates

---
+++ precache-manifest.33d71d67b6496302aeb04ccdf0dc963f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7d3825820fcb57af2277f272c066b3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "19f6462d5e95c11b2a1b",
+    "url": "/react-app/static/js/2.847128fe.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/react-app/static/js/2.847128fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b803851238ea25549f6",
+    "url": "/react-app/static/js/main.8fdc3ca7.chunk.js"
+  },
+  {
+    "revision": "203118a2a0c2cf00fb87",
+    "url": "/react-app/static/js/runtime-main.6430c97e.js"
+  }
+]);
